19607a3c007a535fe64dad98aef91ee767292a85
baiyf <baiyf@canway.net>
2024-10-29 15:50:12 +0800

    feature-删除无效路由

--- apps/monitor/urls.py
+++ apps/monitor/urls.py
@@ -1,13 +1,12 @@
 from rest_framework import routers
 
-from apps.monitor.views.metrics import MetricObjectVieSet, MetricGroupVieSet, MetricVieSet, DimensionVieSet
+from apps.monitor.views.metrics import MetricObjectVieSet, MetricGroupVieSet, MetricVieSet
 from apps.monitor.views.metrics_instance import MetricsInstanceVieSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"api/metrics_object", MetricObjectVieSet, basename="MetricsInstance")
 router.register(r"api/metrics_group", MetricGroupVieSet, basename="MetricsInstance")
 router.register(r"api/metrics", MetricVieSet, basename="MetricsInstance")
-router.register(r"api/metrics_dimension", DimensionVieSet, basename="MetricsInstance")
 router.register(r"api/metrics_instance", MetricsInstanceVieSet, basename="MetricsInstance")
 
 urlpatterns = router.urls
