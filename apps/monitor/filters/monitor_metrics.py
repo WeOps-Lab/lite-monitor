18ee0b7975a8df42860bad67fdbe8e0456b8a7d6
baiyufei <baiyfyx@163.com>
2024-11-07 15:08:19 +0800

    feature-支持id和name查询监控指标与分组列表

--- apps/monitor/filters/monitor_metrics.py
+++ apps/monitor/filters/monitor_metrics.py
@@ -4,16 +4,18 @@ from apps.monitor.models.monitor_metrics import MetricGroup, Metric
 
 
 class MetricGroupFilter(FilterSet):
-    monitor_object = CharFilter(field_name="monitor_object__name", lookup_expr="exact", label="指标对象名称")
+    monitor_object_name = CharFilter(field_name="monitor_object__name", lookup_expr="exact", label="指标对象名称")
+    monitor_object_id = CharFilter(field_name="monitor_object_id", lookup_expr="exact", label="指标对象ID")
 
     class Meta:
         model = MetricGroup
-        fields = ["monitor_object"]
+        fields = ["monitor_object_name", "monitor_object_id"]
 
 
 class MetricFilter(FilterSet):
-    monitor_object = CharFilter(field_name="monitor_object__name", lookup_expr="exact", label="指标对象名称")
+    monitor_object_name = CharFilter(field_name="monitor_object__name", lookup_expr="exact", label="指标对象名称")
+    monitor_object_id = CharFilter(field_name="monitor_object_id", lookup_expr="exact", label="指标对象ID")
 
     class Meta:
         model = Metric
-        fields = ["monitor_object"]
+        fields = ["monitor_object_name", "monitor_object_id"]
