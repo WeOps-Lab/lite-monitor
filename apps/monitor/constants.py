f676310197a3c12f546977e3a3b45e390c6defc6
baiyufei <baiyfyx@163.com>
2024-11-12 15:39:59 +0800

    feature-修改监控对象默认指标

--- apps/monitor/constants.py
+++ apps/monitor/constants.py
@@ -7,7 +7,7 @@ VICTORIAMETRICS_PWD = os.getenv("VICTORIAMETRICS_PWD")
 
 # 内置的监控对象
 MONITOR_OBJS = [
-    {"type": "OS", "name": "Host", "default_metric": "node_cpu_guest_seconds_total_counter"},
+    {"type": "OS", "name": "Host", "default_metric": "node_cpu_seconds_total_counter"},
     {"type": "Web", "name": "Website", "default_metric": "probe_http_status_code_gauge"},
-    {"type": "K8S", "name": "K8S", "default_metric": "kube_namespace_labels_gauge"},
+    {"type": "K8S", "name": "K8S", "default_metric": "internal_write_write_time_ns"},
 ]
