40f264dbbd2600c84bbb1a7b758afec254699ad9
baiyufei <baiyfyx@163.com>
2024-11-20 17:41:32 +0800

    feature-修改监控策略逻辑

--- apps/monitor/models/monitor_policy.py
+++ apps/monitor/models/monitor_policy.py
@@ -16,13 +16,13 @@ class MonitorPolicy(TimeInfo, MaintainerInfo):
     name = models.CharField(max_length=100, verbose_name='监控策略名称')
     organizations = models.JSONField(default=list, verbose_name='所属组织')
     source = models.JSONField(default=dict, verbose_name="策略适用的资源")
-    frequency = models.IntegerField(verbose_name="监控频率(秒)")
-    period = models.IntegerField(verbose_name="检测周期(秒)")
+    frequency = models.IntegerField(verbose_name="监控检测频率(秒)")
+    period = models.IntegerField(verbose_name="每次监控检测的数据周期(秒)")
     algorithm = models.CharField(max_length=10, choices=ALGORITHM_CHOICES, verbose_name="聚合算法")
     threshold = models.JSONField(default=list, verbose_name="阈值")
 
     recovery_condition = models.SmallIntegerField(default=1, verbose_name="多少周期不满足阈值自动恢复")
-    no_data_alert = models.SmallIntegerField(default=1, verbose_name="连续多少周期无数据产生告警")
+    no_data_alert = models.SmallIntegerField(default=1, verbose_name="连续多少周期无数据, 生成告警")
 
     notice = models.BooleanField(default=True, verbose_name="是否通知")
     notice_type = models.CharField(max_length=10, default="email", choices=NOTICE_TYPE_CHOICES, verbose_name="通知方式")
@@ -33,24 +33,31 @@ class MonitorPolicy(TimeInfo, MaintainerInfo):
         verbose_name_plural = '监控策略'
 
 
-class MonitorAlert(TimeInfo):
-    STATUS_CHOICES = [('new', 'New'), ('closed', 'Closed'), ('recovered', 'Recovered')]
+class MonitorEvent(models.Model):
+    LEVEL_CHOICES = [("no_data", "No Data"), ('info', 'Info'), ('warning', 'Warning'), ('error', 'Error'), ('critical', 'Critical')]
 
     policy = models.ForeignKey(MonitorPolicy, on_delete=models.CASCADE, verbose_name='监控策略')
     monitor_instance = models.ForeignKey(MonitorInstance, on_delete=models.CASCADE, verbose_name='监控实例')
-    status = models.CharField(db_index=True, max_length=20, default="new", choices=STATUS_CHOICES, verbose_name='告警状态')
-    operator = models.CharField(max_length=50, verbose_name='操作人')
-
-    class Meta:
-        verbose_name = '监控告警'
-        verbose_name_plural = '监控告警'
-
-
-class MonitorEvent(models.Model):
-    LEVEL_CHOICES = [('info', 'Info'), ('warning', 'Warning'), ('error', 'Error'), ('critical', 'Critical')]
-
-    monitor_alert = models.ForeignKey(MonitorAlert, on_delete=models.CASCADE, verbose_name='事件所属的告警')
     created_at = models.DateTimeField(db_index=True, auto_now_add=True, verbose_name="事件生成时间" )
     value = models.FloatField(verbose_name='事件值')
     level = models.CharField(max_length=20, choices=LEVEL_CHOICES, verbose_name='事件级别')
     notice_result = models.BooleanField(default=True, verbose_name='通知结果')
+
+
+class MonitorAlert(TimeInfo):
+    STATUS_CHOICES = [('new', 'New'), ('closed', 'Closed'), ('recovered', 'Recovered')]
+    ALERT_TYPE_CHOICES = [('alert', 'Alert'), ('no_data', 'No Data')]
+
+    policy = models.ForeignKey(MonitorPolicy, on_delete=models.CASCADE, verbose_name='监控策略')
+    monitor_instance = models.ForeignKey(MonitorInstance, on_delete=models.CASCADE, verbose_name='监控实例')
+    alert_type = models.CharField(default="alert", choices=ALERT_TYPE_CHOICES, max_length=50, verbose_name='告警类型')
+    status = models.CharField(db_index=True, max_length=20, default="new", choices=STATUS_CHOICES, verbose_name='告警状态')
+    start_event_id = models.IntegerField(blank=True, null=True, verbose_name='开始事件ID')
+    start_event_time = models.DateTimeField(blank=True, null=True, verbose_name='开始事件时间')
+    end_event_id = models.IntegerField(blank=True, null=True, verbose_name='结束事件ID')
+    end_event_time = models.DateTimeField(blank=True, null=True, verbose_name='结束事件时间')
+    operator = models.CharField(max_length=50, verbose_name='告警处理人')
+
+    class Meta:
+        verbose_name = '监控告警'
+        verbose_name_plural = '监控告警'
--- apps/monitor/tasks/monitor_policy.py
+++ apps/monitor/tasks/monitor_policy.py
@@ -1,6 +1,7 @@
 import logging
 
 from celery.app import shared_task
+from datetime import datetime, timezone
 
 from apps.monitor.constants import POLICY_METHODS, THRESHOLD_METHODS
 from apps.monitor.models import MonitorPolicy, MonitorInstanceOrganization, MonitorAlert, MonitorEvent
@@ -13,16 +14,22 @@ logger = logging.getLogger("app")
 @shared_task
 def scan_policy():
     """扫描监控策略"""
-    logger.info("Start to update monitor instance grouping rule")
+    logger.info("start to update monitor instance grouping rule")
     policy_objs = MonitorPolicy.objects.all()
     for policy in policy_objs:
         MonitorPolicyScan(policy).run()
-    logger.info("Finish to update monitor instance grouping rule")
+    logger.info("finish to update monitor instance grouping rule")
 
 
 class MonitorPolicyScan:
     def __init__(self, policy):
         self.policy = policy
+        self.instances = self.get_instances()
+        self.active_alerts = self.get_active_alerts()
+
+    def get_active_alerts(self):
+        """获取策略的活动告警"""
+        return MonitorAlert.objects.filter(policy_id=self.policy.id, status="new")
 
     def get_instances(self):
         """获取策略适用的实例"""
@@ -39,18 +46,20 @@ class MonitorPolicyScan:
 
     def query_metrics(self):
         """查询指标"""
-        instances = self.get_instances()
+        step = int(self.policy.period / 10)
+        end_timestamp = int(datetime.now(timezone.utc).timestamp())
+        start_timestamp = end_timestamp - self.policy.frequency
         query = self.policy.query
-        instances_str = "|".join(instances)
+        instances_str = "|".join(self.instances)
         query.replace("__labels__", f"instance_id=~'{instances_str}'")
-        metrics = VictoriaMetricsAPI().query(query)
+        metrics = VictoriaMetricsAPI().query_range(query, start_timestamp, end_timestamp, step)
         return metrics
 
     def metric_aggregation(self, metrics):
         """指标聚合"""
         method = POLICY_METHODS.get(self.policy.algorithm)
         if not method:
-            raise ValueError("Invalid algorithm")
+            raise ValueError("invalid policy method")
         metrics_map = {}
         for metric_info in metrics.get("result", []):
             instance_id = metric_info["metric"].get("instance_id")
@@ -61,17 +70,27 @@ class MonitorPolicyScan:
         result = {instance_id: method(values) for instance_id, values in metrics_map.items()}
         return result
 
-    def compare(self, aggregation_result):
-        """对比结果, 生成告警事件"""
+    def compare_event(self, aggregation_result):
+        """比较事件"""
         compare_result = []
 
+        # 计算无数据事件
+        for instance_id in self.instances:
+            if instance_id not in aggregation_result:
+                compare_result.append({
+                    "instance_id": instance_id,
+                    "value": 0,
+                    "level": "no_data",
+                })
+
+        # 计算告警事件
         for instance_id, value in aggregation_result.items():
 
             event = None
             for threshold_info in self.policy.threshold:
                 method = THRESHOLD_METHODS.get(threshold_info["method"])
                 if not method:
-                    raise ValueError("Invalid threshold method")
+                    raise ValueError("invalid threshold method")
                 if method(value, threshold_info["value"]):
                     event = {
                         "instance_id": instance_id,
@@ -80,6 +99,7 @@ class MonitorPolicyScan:
                     }
                     break
 
+            # 未触发阈值的事件
             if not event:
                 event = {
                     "instance_id": instance_id,
@@ -91,81 +111,121 @@ class MonitorPolicyScan:
 
         return compare_result
 
-    def event_handling(self, events: list):
-        """事件处理"""
-        alert_instance_map = self.create_or_get_alert(events)
-        for event in events:
-            event.update(monitor_alert_id=alert_instance_map[event["instance_id"]])
-
-        # 创建告警事件
-        self.create_event(events)
-
-        # 处理告警恢复
-        self.recovery(events)
-
-    def create_or_get_alert(self, events):
-        """创建或获取告警"""
-        new_event_instance_ids = [event["instance_id"] for event in events]
-        alert_objs = MonitorAlert.objects.filter(
-            policy_id=self.policy.id, status="new",monitor_instance_id__in=new_event_instance_ids)
-        alert_instance_map = {alert.monitor_instance_id: alert.id for alert in alert_objs}
-        new_alerts = set(new_event_instance_ids) - set(alert_instance_map.keys())
-        new_alert_creates = [
-            MonitorAlert(policy_id=self.policy.id, monitor_instance_id=instance_id, status="new")
-            for instance_id in new_alerts
-        ]
-        new_alert_objs = MonitorAlert.objects.bulk_create(new_alert_creates, batch_size=200)
-        for alert in new_alert_objs:
-            alert_instance_map[alert.monitor_instance_id] = alert.id
-        return alert_instance_map
-
     def create_event(self, events):
         """创建事件"""
         new_event_creates = [
             MonitorEvent(
-                monitor_alert_id=event["monitor_alert_id"],
+                policy_id=self.policy.id,
+                monitor_instance_id=event["instance_id"],
                 value=event["value"],
                 level=event["level"],
-                notice_result=True
+                notice_result=True,
             )
             for event in events
         ]
-        MonitorEvent.objects.bulk_create(new_event_creates, batch_size=200)
-
-    def recovery(self, events):
-        """告警恢复"""
-        need_check_events = [event for event in events if event["level"] == "info"]
-
-        recovery_condition = self.policy.recovery_condition
-
-        alerts = MonitorAlert.objects.filter(
-            id__in=[event["monitor_alert_id"] for event in need_check_events]).prefetch_related("monitorevent_set")
-
-        recovery_alerts = []
-
-        for alert in alerts:
-            event_objs = alert.monitorevent_set.all().order_by("-created_at")
-            count = 0
-            for event_obj in event_objs:
-                if event_obj.level != "info":
-                    break
-                count += 1
-            if count >= recovery_condition:
-                alert.status = "recovered"
-                recovery_alerts.append(alert)
-
-        MonitorAlert.objects.bulk_update(recovery_alerts, ["status"], batch_size=200)
+        event_objs = MonitorEvent.objects.bulk_create(new_event_creates, batch_size=200)
+        return event_objs
 
     def notice(self, events):
         """通知"""
         # todo 告警通知
         for event in events:
-            logger.info(f"Instance {event['instance_id']} has a new event, value is {event['value']}")
-        pass
+            logger.info(f"to {self.policy.notice_users}，instance {event['instance_id']} has a new event, value is {event['value']}")
+
+    def recovery_alert(self, event_objs):
+        """告警恢复处理"""
+
+        event_objs_map = {event.monitor_instance_id: event for event in event_objs}
+        recovery_alerts = []
+        for alert in self.active_alerts:
+            event_obj = event_objs_map.get(alert.monitor_instance_id)
+            if alert.alert_type == "no_data":
+                # 无数据告警恢复
+                if event_obj.level != "no_data":
+                    alert.status = "recovered"
+                    alert.end_event_id = event_obj.id
+                    alert.end_event_time = event_obj.created_at
+                    recovery_alerts.append(alert)
+            else:
+                # 正常告警恢复
+                if event_obj.level == "info":
+                    events = MonitorEvent.objects.filter(
+                        policy_id=self.policy.id,
+                        monitor_instance_id=event_obj.monitor_instance_id,
+                    ).order_by("-created_at")[:self.policy.recovery_condition]
+                    if all(event.level == "info" for event in events):
+                        alert.status = "recovered"
+                        alert.end_event_id = event_obj.id
+                        alert.end_event_time = event_obj.created_at
+                        recovery_alerts.append(alert)
+        MonitorAlert.objects.bulk_update(recovery_alerts, ["status", "end_event_id", "end_event_time"], batch_size=200)
+
+    def create_alert(self, event_objs):
+        """告警生成处理"""
+        no_data_events, alert_events = [], []
+        for event_obj in event_objs:
+            if event_obj.level == "info":
+                continue
+            if event_obj.level == "no_data":
+                no_data_events.append(event_obj)
+            else:
+                alert_events.append(event_obj)
+
+        # 正常告警
+        alert_objs = [
+            MonitorAlert(
+                policy_id=self.policy.id,
+                monitor_instance_id=event_obj.monitor_instance_id,
+                alert_type="alert",
+                status="new",
+                start_event_id=event_obj.id,
+                start_event_time=event_obj.created_at,
+                operator="system",
+            )
+            for event_obj in alert_events
+        ]
+
+        # 无数据告警
+        for event_obj in no_data_events:
+            events = MonitorEvent.objects.filter(
+                policy_id=self.policy.id,
+                monitor_instance_id=event_obj.monitor_instance_id,
+            ).order_by("-created_at")[:self.policy.no_data_alert]
+            if all(event.level == "no_data" for event in events):
+                alert_objs.append(
+                    MonitorAlert(
+                        policy_id=self.policy.id,
+                        monitor_instance_id=event_obj.monitor_instance_id,
+                        alert_type="no_data",
+                        status="new",
+                        start_event_id=event_obj.id,
+                        start_event_time=event_obj.created_at,
+                        operator="system",
+                    )
+                )
+        MonitorAlert.objects.bulk_create(alert_objs, batch_size=200)
+
+    def alert_handling(self, event_objs):
+        """告警处理"""
+
+        active_alert_map = {alert.monitor_instance_id for alert in self.active_alerts}
+        recovery_alert_events, create_alert_events = [], []
+        for event_obj in event_objs:
+            if event_obj.monitor_instance_id in active_alert_map:
+                recovery_alert_events.append(event_obj)
+            else:
+                create_alert_events.append(event_obj)
+
+        # 告警恢复处理
+        self.recovery_alert(recovery_alert_events)
+
+        # 告警生成处理
+        self.create_alert(create_alert_events)
 
     def run(self):
         """运行"""
         metrics = self.query_metrics()
         aggregation_result = self.metric_aggregation(metrics)
-        events = self.compare(aggregation_result)
-        self.event_handling(events)
+        events = self.compare_event(aggregation_result)
+        event_objs = self.create_event(events)
+        self.alert_handling(event_objs)
